b24dece7cf93a28505cee4f7fd20ef5eb5d0e60d
Ghoneim <ahmed.ghoneim1011@gmail.com>
2024-02-06 05:35:31 +0200

    adding another commit to try and merge with the master

--- app/src/main/java/com/example/aklah/MainActivity.java
+++ app/src/main/java/com/example/aklah/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String s = "hello world";
+        int we = 5;
     }
 }
